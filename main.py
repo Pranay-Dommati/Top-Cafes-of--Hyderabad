35ec35eb24c083a765d3570e80ac8439eca4b503
PranayDommati <bannydommati@gmail.com>
2024-04-08 12:12:47 +0530

    1st commit

--- main.py
+++ main.py
@@ -1,13 +1,9 @@
 from flask import Flask, render_template, request
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
-from sqlalchemy import Integer, String, Boolean
-from flask_bootstrap import Bootstrap5
 import os
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = os.environ.get('secret_key')
-Bootstrap5(app)
 # Connect to Database
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('database_uri')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
